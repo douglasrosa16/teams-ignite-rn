0dea4f960f6f5b1e56b3f48c545c8d8ed4577369
douglasrosaunisystem <76063406+DouglasUnisystem@users.noreply.github.com>
2023-07-27 07:44:15 -0400

    feat: fonts loaded

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
-import Groups from '@screens/Groups';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
 
+import { Loading }  from '@components/Loading';
+import Groups from '@screens/Groups';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
