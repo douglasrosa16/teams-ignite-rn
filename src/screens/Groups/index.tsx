8652d3eb7ff56c22ef3a5c2bc88c867301fd80fe
douglasrosaunisystem <76063406+DouglasUnisystem@users.noreply.github.com>
2023-07-28 07:50:07 -0400

    styled: add logo

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,10 @@
+import { Header } from '@components/Header';
 import { Container, Title } from './styles';
 
 export default function Groups() {
   return (
     <Container>
-      <Title>
-        Groups
-      </Title>
+      <Header />
     </Container>
   );
 }
