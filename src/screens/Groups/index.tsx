ebced46283d3a51a39d2dd9cb1b701dcf99d73bf
douglasrosaunisystem <76063406+DouglasUnisystem@users.noreply.github.com>
2023-07-31 07:36:43 -0400

    feat: component EmptyList

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
+import { FlatList, Text } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Hightlight } from '@components/Hightlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles';
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
@@ -12,8 +18,21 @@ export default function Groups() {
         title="Turmas"
         subtitle="jogue com a sua turma"
       />
-
-      <GroupCard title="Galera do ignite" />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Que tal cadastrar a primeira turma?"
+          />
+        )}
+      />     
     </Container>
   );
 }
