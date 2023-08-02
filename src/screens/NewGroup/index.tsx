13eecc83bd9a5f60895c6162779248ecfc4e530b
douglasrosaunisystem <76063406+DouglasUnisystem@users.noreply.github.com>
2023-08-02 07:36:14 -0400

    feat: component input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon} from './styles';
 
 import { Header } from '@components/Header';
 import { Button } from '@components/Button';
+import { Input } from '@components/Input';
 
 export function NewGroup(){
   return (
@@ -17,7 +18,10 @@ export function NewGroup(){
           subtitle="crie a turma para adicionar as pessoas"
         />
 
+        <Input />
+
         <Button 
+          style={{ marginTop: 20 }}
           title="Criar"
         />
       </Content>
