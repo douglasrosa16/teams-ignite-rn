c20c76830ea54215410ebd98b9ff60488ae5e8fe
douglasrosaunisystem <76063406+DouglasUnisystem@users.noreply.github.com>
2023-08-02 12:51:42 -0400

    feat: criando componente buttonIcon

---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,20 @@
+import { Header } from '@components/Header';
+import { Hightlight } from '@components/Hightlight';
+import { ButtonIcon } from '@components/ButtonIcon';
+
+import { Container } from './styles';
+
+export function Players(){
+  return (
+    <Container>
+      <Header showBackButton/>
+
+      <Hightlight
+        title="Nome da turma"
+        subtitle="adicione a galera e separe os times"
+      />
+
+      <ButtonIcon />
+    </Container>
+  )
+}
