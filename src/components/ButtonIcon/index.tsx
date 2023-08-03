86dc8cf22ec310a8a8dfb76dfac96f7eed98ea9e
douglasrosaunisystem <76063406+DouglasUnisystem@users.noreply.github.com>
2023-08-03 07:45:52 -0400

    feat: componente fitler

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,15 +1,24 @@
 import { TouchableOpacityProps } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-import { Container, Icon } from './styles';
+import { Container, Icon, ButtonIconTypeStyleProps } from './styles';
 
 type Props = TouchableOpacityProps & {
-
+  icon: keyof typeof MaterialIcons.glyphMap; //tipo de dados dos ícones para deixar mais flexível
+  type?: ButtonIconTypeStyleProps;
 }
 
-export function ButtonIcon({  } : Props){
+export function ButtonIcon({ icon, type = 'PRIMARY', ...rest } : Props){
   return (
-    <Container>
-      <Icon name="home"/>
+    <Container {...rest}>
+      <Icon 
+        name={icon}
+        type={type}
+      />
     </Container>
   )
 }
+
+
+
+
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,20 +1,36 @@
 import { Header } from '@components/Header';
 import { Hightlight } from '@components/Hightlight';
 import { ButtonIcon } from '@components/ButtonIcon';
+import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
 
-import { Container } from './styles';
+import { Container, Form } from './styles';
 
-export function Players(){
+export function Players() {
   return (
     <Container>
-      <Header showBackButton/>
+      <Header showBackButton />
 
       <Hightlight
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
 
-      <ButtonIcon />
+      <Form>
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+
+        <ButtonIcon
+          icon="add"
+        />
+      </Form>
+
+      <Filter 
+        title="Time A"
+        isActive
+      />
     </Container>
   )
 }
