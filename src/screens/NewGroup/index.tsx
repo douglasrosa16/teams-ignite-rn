2eb5d3a6ea80296cd420fedd821c913391aa8567
douglasrosa16 <42454362+douglasrosa16@users.noreply.github.com>
2023-08-16 16:34:09 -0400

    feat: classe de error

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,48 +1,59 @@
 import { useState } from 'react';
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { AppError } from '@utils/AppError';
 
 import { groupCreate } from '@storage/group/groupCreate';
 
 import { Hightlight } from '@components/Hightlight';
-import { Container, Content, Icon} from './styles';
+import { Container, Content, Icon } from './styles';
 
 import { Header } from '@components/Header';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 
-export function NewGroup(){
+export function NewGroup() {
   const [group, setGroup] = useState('');
 
   const navigation = useNavigation();
 
-  async function handleNew(){
+  async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo grupo', 'Informe o nome da  turma');
+      }
+
       await groupCreate(group);
 
-      navigation.navigate('players',{ group });  
+      navigation.navigate('players', { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert('Novo grupo', error.message);
+      } else {
+        Alert.alert('Novo grupo', 'Não foi possível criar um novo grupo.');
+        console.log(error);
+      }
     }
   }
 
   return (
     <Container>
-      <Header showBackButton/>
+      <Header showBackButton />
 
       <Content>
         <Icon />
 
-        <Hightlight 
+        <Hightlight
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Input 
+        <Input
           placeholder="Nome da turma"
           onChangeText={setGroup}
         />
 
-        <Button 
+        <Button
           style={{ marginTop: 20 }}
           title="Criar"
           onPress={handleNew}
--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,16 +1,23 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AppError } from '@utils/AppError';
 
 import { GROUP_COLLECTION } from '@storage/storageConfig';
 import { groupsGetAll } from './groupsGetAll';
 
-export async function groupCreate(newGroup: string){
+export async function groupCreate(newGroup: string) {
   try {
-     const storedGroups = await groupsGetAll();
+    const storedGroups = await groupsGetAll();
+
+    const groupAlredyExists = storedGroups.includes(newGroup);
+
+    if (groupAlredyExists){
+      throw new AppError('Já existe um grupo cadastrado com esse nome.') ;
+    }
 
     const storage = JSON.stringify([...storedGroups, newGroup]);
 
     await AsyncStorage.setItem(GROUP_COLLECTION, storage);
-  } catch(error){
+  } catch (error) {
     throw error;
   }
 }
